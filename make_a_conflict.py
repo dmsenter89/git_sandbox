34fae1c6212f8ff3038a27c206a828678a6274b7
Michael Senter <dmsenter89@gmail.com>
2020-01-13 14:29:02 -0500

    changed numbers

--- make_a_conflict.py
+++ make_a_conflict.py
@@ -4,14 +4,16 @@ from typing import List
 
 def print_sqrs(nums: List[float]):
     """Takes a list of floats and prints the square of each element."""
+    print('--------------------')
     for elem in nums:
         print(f"{elem}^2 == {elem**2}")
+    print('--------------------')
 
 def some_function():
     pass
 
 def main():
-    some_nums = [1,2,3,4]
+    some_nums = [4,8,12,4]
     print_sqrs(some_nums)
 
 if __name__=='__main__':
